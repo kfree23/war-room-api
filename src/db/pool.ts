49c5c155358fb5e2489b6cf77e6e0bcd6d402b77
kfree23 <krystalfreeman50@gmail.com>
2026-08-12 22:31:53 -0500

    set up postgresql pool for reusable database connections

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,4 +1,7 @@
 import { Pool } from "pg";
+import 'dotenv/config'
 
-// TODO(krystal): you write this
-// - instantiate and export a pg Pool using DATABASE_URL
+const pool = new Pool({ connectionString: process.env.DATABASE_URL});
+
+
+export default pool;
