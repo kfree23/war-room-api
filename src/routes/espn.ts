0fe3197435a4615197a6b1f948f533d1e86ac54a
kfree23 <krystalfreeman50@gmail.com>
2026-09-10 11:26:05 -0500

    add GET requests for roster and athlete

--- src/routes/espn.ts
+++ src/routes/espn.ts
@@ -6,15 +6,48 @@ router.get("/teams", async (req, res) => {
     try {
         const response = await fetch("https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams");
 
-        if(!response.ok) {
+        if (!response.ok) {
             throw new Error(`Something went wrong ${response.status}`)
         }
         const data = await response.json();
-        res.json(data)
+        res.json(data.sports[0].leagues[0].teams)
 
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: 'Failed to fetch.' })
+    }
+})
+
+router.get("/teams/:teamId/roster", async (req, res) => {
+    try {
+        const { teamId } = req.params;
+        const response = await fetch(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/${teamId}/roster`);
+
+        if (!response.ok) {
+            throw new Error(`Something went wrong ${response.status}`)
+        }
+
+        const data = await response.json()
+        res.json(data.athletes)
     } catch(err) {
         console.error(err)
-        res.status(500).json({error: 'There is something wrong with your network.'})
+        res.status(500).json({error: "Failed to fetch."})
+    }
+})
+
+router.get("/athletes/:athleteId", async (req, res) => {
+    try {
+        const { athleteId } = req.params;
+        const response = await fetch(`https://site.web.api.espn.com/apis/common/v3/sports/basketball/nba/athletes/${athleteId}`)
+
+        if (!response.ok) {
+            throw new Error(`Something went wrong ${response.status}`)
+        }
+        const data = await response.json()
+        res.json(data.athlete)
+    } catch(err) {
+        console.error(err)
+        res.status(500).json({error: "Failed to fetch."})
     }
 })
 
